cafbe5f6bf6bf6b749f3de4687ddb413c8b5e5e0
notkahuate <ksbuenors@gmail.com>
2025-04-22 22:54:39 -0500

    feat: :sparkles: añado nuevas mejoras al proyecto

--- js/app.js
+++ js/app.js
@@ -11,3 +11,39 @@ function typeEffect() {
 }
 
 setInterval(typeEffect, 200); // Ajusta la velocidad según desees
+
+document.addEventListener('DOMContentLoaded', function() {
+    const projectCards = document.querySelectorAll('.project-card');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    projectCards.forEach(card => {
+        observer.observe(card);
+    });
+
+    const skillItems = document.querySelectorAll('.about_me .skill-item');
+
+    const skillObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    skillItems.forEach(item => {
+        skillObserver.observe(item);
+    });
+});
